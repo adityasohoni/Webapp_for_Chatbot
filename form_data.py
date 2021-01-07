d02676c5f5ab679d9e5bf08fb3b0ed7ec62c9e08
adityasohoni <adityasohoni@gmail.com>
2021-01-07 17:29:45 +0530

    First commit

--- form_data.py
+++ form_data.py
@@ -20,43 +20,43 @@ def contact_page():
 def charts_page():
     return render_template("charts.html",result=output)
 
-@app.route('/cam')
-def sign_page():
-    return render_template("camera.html")
+# @app.route('/cam')
+# def sign_page():
+#     return render_template("camera.html")
 
 
-@app.route('/camera',methods=['POST'])
-def camera():
-    cap=cv2.VideoCapture(0)
-    while True:
-        ret,img=cap.read()
-        img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-        cv2.imwrite("static/cam.png",img)
+# @app.route('/camera',methods=['POST'])
+# def camera():
+#     cap=cv2.VideoCapture(0)
+#     while True:
+#         ret,img=cap.read()
+#         img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+#         cv2.imwrite("static/cam.png",img)
 
-        # return render_template("camera.html",result=)
-        time.sleep(0.1)
-        return json.dumps({'status': 'OK', 'result': "static/cam.png"})
-        if cv2.waitKey(0) & 0xFF ==ord('q'):
-            break
-    cap.release()
-    # file="/home/ashish/Downloads/THOUGHT.png"
-    # with open(file,'rb') as file:
-    #     image=base64.encodebytes(file.read())
-    #     print(type(image))
-    # return json.dumps({'status': 'OK', 'user': user, 'pass': password});
-    return json.dumps({'status': 'OK', 'result': "static/cam.png"});
+#         # return render_template("camera.html",result=)
+#         time.sleep(0.1)
+#         return json.dumps({'status': 'OK', 'result': "static/cam.png"})
+#         if cv2.waitKey(0) & 0xFF ==ord('q'):
+#             break
+#     cap.release()
+#     # file="/home/ashish/Downloads/THOUGHT.png"
+#     # with open(file,'rb') as file:
+#     #     image=base64.encodebytes(file.read())
+#     #     print(type(image))
+#     # return json.dumps({'status': 'OK', 'user': user, 'pass': password});
+#     return json.dumps({'status': 'OK', 'result': "static/cam.png"});
 
-def gen(camera):
-    while True:
-        data= camera.get_frame()
+# def gen(camera):
+#     while True:
+#         data= camera.get_frame()
 
-        frame=data[0]
-        yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+#         frame=data[0]
+#         yield (b'--frame\r\n'
+#                b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
-@app.route('/video_feed')
-def video_feed():
-    return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
+# @app.route('/video_feed')
+# def video_feed():
+#     return Response(gen(VideoCamera()), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 
 @app.route('/result',methods=["POST","GET"])
@@ -70,10 +70,11 @@ def Result():
             try:
                 r = requests.post('http://localhost:5002/webhooks/rest/webhook', json={"message": result})
                 print("Bot says, ")
+                output.extend([("message parker",result)])
                 for i in r.json():
                     bot_message = i['text']
                     print(f"{i['text']}")
-                output.extend([("message parker",result),("message stark",bot_message)])
+                    output.extend([("message stark",bot_message)])
             except:
                 output.extend([("message parker", result), ("message stark", "We are unable to process your request at the moment. Please try again...")])
 
