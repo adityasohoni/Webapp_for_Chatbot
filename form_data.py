b5e2796ff360e83033ae8805756f160052847488
Rakshit <rakki22k@gmail.com>
2021-02-27 20:00:58 +0530

    resolve merge conflicts

--- form_data.py
+++ form_data.py
@@ -12,9 +12,7 @@ output=[]
 map = []
 
 def defaultCoordinates():
-    lat.append(13.010992210852605)
-    lng.append(74.79431669717836)
-    # coordinates of NITK Surathkal
+    
     
     map.append(embed["NITKSurathkal"])
 
@@ -22,35 +20,7 @@ defaultCoordinates()
 
 def getCoordinates(location):
     
-    map = {
-        
-        "Amul" : [13.008990937415803,74.79676318694564],
-        "Nescafe" : [13.007456649901386, 74.79686072103866],
-        "Nandini" : [13.012629093163248, 74.79609699793872],
-        "Mudrika" : [13.008615710447698, 74.79415678177236],
-        "MainBuilding" : [13.010816520025335, 74.79430667015167],
-        "OceanPearl" : [13.008555277616512, 74.79518139773533],
-        "Yennar" : [13.008662050867967, 74.79410813883005],
-        "HealthCareCentre" : [13.009520241237365, 74.79624759779159],
-        "SportsComplex" : [13.009854915350024, 74.79811174202523],
-        "SBIBank" : [13.008954404207831, 74.79411792717062],
-        "CCC" : [13.009407250859917, 74.79579061551671],
-        "CentralLibrary" : [13.010511730820618, 74.79436580937538],
-
-        "ITDepartment" : [13.01096341447703, 74.7921926013909],
-        "CSEDepartment" : [13.01277483761104, 74.79135221551678],
-        "ECEDepartment" : [13.011635697504223, 74.79215169770643],
-        "EEEDepartment" : [13.0109677794616, 74.79231137590178],
-        "MECHDepartment" : [13.012189357491657, 74.79591750645949],
-        "MNDepartment" : [13.011766176141933, 74.7939723577556],
-        "METADepartment" : [13.010431655743487, 74.79498642708732],
-        "CVDepartment" : [13.01224358420148, 74.79399369205241],
-        "CHEMDepartment" : [13.011187633263118, 74.79623468581308]
-        
-    }
-
-    lat.append(map[location][0])
-    lng.append(map[location][1])
+    
     map.append(embed[location])
     
     
@@ -105,8 +75,7 @@ def Result():
                    
             except:
                 output.extend([("message user", result), ("message bot", "We are unable to process your request at the moment. Please try again...")])
-        print(lat) 
-        print(output)
+        
         return render_template("IY_Home_page.html",result=output,embed_map = map[-1])
 
 if __name__=="__main__":
